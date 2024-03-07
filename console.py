d13d3c9d15b8dd2b45d20b743c6851c9ee798d36
Jaaf4r <jaafar.bouhnine@gmail.com>
2024-03-07 19:51:38 +0100

    console 0.1

--- console.py
+++ console.py
@@ -1,70 +1,187 @@
 #!/usr/bin/python3
 """ Console """
 import cmd
-import os
+import json
+import shlex
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Console class"""
+    """HBNBCommand Class"""
 
-    prompt = '(hbnb) '  # custom prompt
+    prompt = '(hbnb) '
 
-    def do_help(self, line):
+    def do_quit(self, arg):
+        """Quit command to exit the program\n"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program\n"""
+        print()
+        return True
+
+    def do_create(self, arg):
         """
-            this method invoked when the user types 'help'
-            if line is exists will show a msg, if it's not
-            it'll show another msg
+        Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id.
         """
+        if not arg:
+            print("** class name missing **")
+            return
 
-        if line.strip() == "quit":
-            self.help_quit()
+        class_name = arg.split()[0]
+
+        try:
+            class_instance = globals()[class_name]()
+            class_instance.save()
+            print(class_instance.id)
+        except KeyError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id.
+        """
+        args = arg.split()
+        objdict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in globals() or \
+                not issubclass(globals()[args[0]], BaseModel):
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in objdict:
+            print("** no instance found **")
         else:
-            print("\nDocumented commands (type help <topic>):\
-            \n{}\nEOF  help  quit\n".format('=' * 40))
+            print(objdict["{}.{}".format(args[0], args[1])])
 
-    def help_quit(self):
+    def do_destroy(self, arg):
         """
-            method print a string when invoked
+        Deletes an instance based on the class name
+        and id (save the change into the JSON file)
         """
+        args = arg.split()
+        if arg == '':
+            print('** class name missing **')
+        elif args[0] not in globals() or \
+                not issubclass(globals()[args[0]], BaseModel):
+            print('** class doesn\'t exist **')
+        else:
+            if len(args) < 2:
+                print('** instance id missing **')
+            else:
+                classname = args[0]
+                objid = args[1]
+                key = classname + '.' + objid
+                try:
+                    del storage.all()[key]
+                    storage.save()
+                except KeyError:
+                    print('** no instance found **')
 
-        print("Quit command to exit the program\n")
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances
+        based or not on the class name. Ex: $ all BaseModel or $ all
+        """
+        args = arg.split()
+        result = []
+        if len(args) != 0:
+            if args[0] not in globals() or \
+                    not issubclass(globals()[args[0]], BaseModel):
+                print('** class doesn\'t exist **')
+                return
+            else:
+                for key, value in storage.all().items():
+                    if type(value).__name__ == args[0]:
+                        result.append(value.__str__())
+        else:
+            for key, value in storage.all().items():
+                result.append(value.__str__())
+        print(result)
 
-    def do_quit(self, line):
-        """
-            this method is invoked when the user types 'quit'
-            this method call another method to exists the console
-        """
-        self.quitted()
+    def do_update(self, arg):
+        ''' Updates an instance based on the class name & id adding/updating
+            attribute (save the change into the JSON file). Ex: $ update
+            BaseModel 1234-1234-1234 email "aibnb@holbertonschool.com". '''
 
-    def emptyline(self):
-        """
-            this method called when the user enters empty line
-            without this method we'll have in most cases error
-        """
-        pass
+        if not arg:
+            print("** class name missing **")
+            return
+        args = shlex.split(arg)
+        storage.reload()
+        obj = storage.all()
 
-    def do_cl(self, line):
-        """
-            method to clear the function by using 'cl' instead of 'clear'
-            'cl': command used to clear the console
-        """
-        os.system('cls' if os.name == 'nt' else 'clear')
+        if args[0] not in globals() or \
+                not issubclass(globals()[args[0]], BaseModel):
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        try:
+            obj_key = args[0] + "." + args[1]
+            obj[obj_key]
+        except KeyError:
+            print("** no instance found **")
+            return
+        if (len(args) == 2):
+            print("** attribute name missing **")
+            return
+        if (len(args) == 3):
+            print("** value missing **")
+            return
+        obj_dict = obj[obj_key].__dict__
+        if args[2] in obj_dict.keys():
+            d_type = type(obj_dict[args[2]])
+            print(d_type)
+            obj_dict[args[2]] = type(obj_dict[args[2]])(args[3])
+        else:
+            obj_dict[args[2]] = args[3]
+        storage.save()
 
-    def default(self, line):
-        """
-            This method is called when the user enters
-            an unknown command, including EOF.
-        """
-        if line == 'EOF':
-            print("\n", end='')
-            self.quitted()
+    def do_update2(self, arg):
+        ''' Updates an instance based on the class name & id adding/updating
+            attribute (save the change into the JSON file). Ex: $ update
+            BaseModel 1234-1234-1234 email "aibnb@holbertonschool.com". '''
 
-    def quitted(self):
-        """
-            method to exit the console
-        """
+        if not arg:
+            print("** class name missing **")
+            return
+        my_dict = "{" + arg.split("{")[1]
+        args = shlex.split(arg)
+        storage.reload()
+        obj = storage.all()
 
-        exit()
+        if args[0] not in globals() or \
+                not issubclass(globals()[args[0]], BaseModel):
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        try:
+            obj_key = args[0] + "." + args[1]
+            obj[obj_key]
+        except KeyError:
+            print("** no instance found **")
+            return
+        if (my_dict == "{"):
+            print("** attribute name missing **")
+            return
+        my_dict = my_dict.replace("\'", "\"")
+        my_dict = json.loads(my_dict)
+        obj_inst = obj[obj_key]
+        for k in my_dict:
+            if hasattr(obj_inst, k):
+                d_type = type(getattr(obj_inst, k))
+                setattr(obj_inst, k, my_dict[k])
+            else:
+                setattr(obj_inst, k, my_dict[k])
+        storage.save()
 
 
 if __name__ == '__main__':
