81d555ec33fe8b559fe9cf9c53093d3bc4c57361
ismael <spacingismael@gmail.com>
2024-03-09 13:10:53 +0100

    test base_model.py requirements

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """
 BaseModel
 """
+
 import models
 import uuid
 from datetime import datetime
@@ -12,10 +13,12 @@ class BaseModel():
     Class BaseModel that defines all common
     attributes/methods for other classes.
     """
+
     def __init__(self, *args, **kwargs):
         """ creating BaseModel from dictionary if kwargs exist,
             otherwise creating id & create_at (new instance)
         """
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at":
@@ -36,6 +39,7 @@ class BaseModel():
         Returns the string representation
         of the instance.
         """
+
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
@@ -44,6 +48,7 @@ class BaseModel():
         Updates the public instance attribute
         updated_at with the current datetime.
         """
+
         self.updated_at = datetime.now()
         models.storage.save()
 
@@ -52,6 +57,7 @@ class BaseModel():
         Returns a dictionary containing all keys/values
         of __dict__ of the instance.
         """
+
         dict = {**self.__dict__}
         dict['__class__'] = self.__class__.__name__
         dict['created_at'] = self.created_at.isoformat()
